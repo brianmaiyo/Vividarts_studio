f74c0a45051b2cb61350f84ad5c07a88220c5b4d
Anthony Kioko <102463336+Tonny-Kioko@users.noreply.github.com>
2023-12-22 11:41:58 +0300

    Aws credentials edit

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "A"
-  secret_key = "V"
+  access_key = "$youraccesskey"
+  secret_key = "$yoursecretkey"
 }
 
 #Creating a VPC
